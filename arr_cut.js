6f955f3b4b459b08fc10d988eebc1aa0fb041b4f
jungdaehun1024 <141194373+jungdaehun1024@users.noreply.github.com>
2023-11-24 10:58:40 +0900

    Revert "Revert "배열자르기""
    This reverts commit ed78e626082a9ec4ed4fed8d6096db234f360a50.

--- arr_cut.js
+++ arr_cut.js
@@ -1,4 +1,21 @@
 //배열자르기
-function solution(numbers, num1, num2) {
-  return numbers.filter((v, i) => i >= num1 && i <= num2);
+// function solution(numbers,num1,num2){
+//   const answer = numbers.filter((cur_val,cur_idx)=>{
+//     if(cur_idx >=num1 && cur_idx <=num2){
+//       return cur_val;
+//     }
+   
+//   })
+//  return answer;
+// }
+
+
+
+function solution(numbers,num1,num2){
+  const answer = numbers.filter((cur_val,cur_idx)=>{
+    return cur_idx >=num1 && cur_idx <=num2 ;
+  })
+
+   return answer;
 }
+solution([1,2,3,4,5],1,3);
